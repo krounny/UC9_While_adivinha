0b9e41830af500cd5205ee0eb6f1e1ac9168674c
LUIZ EDUARDO CANELA RIVERA <luiz.ecrivera@senacsp.edu.br>
2023-03-01 22:00:35 -0300

    Criando repositorio para o projeto - While adivinha

---
+++ UC9_While_adivinha/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string palavrasecreta, palavrausuario;
+int tentativas;
+
+palavrasecreta = "fedida";
+tentativas = 3;
+
+while (tentativas > 0)
+{
+    Console.Clear();
+    Console.WriteLine("A Luisa é: \n " + "Você ainda tem " + tentativas + " tentativas! \n\n");
+
+    Console.WriteLine("Digite a palavra: ");
+    palavrausuario = Console.ReadLine();
+
+    if (palavrausuario == palavrasecreta)
+    {
+        Console.WriteLine("Ela é mesmo!!!");
+        tentativas = 0;
+    }
+    else
+    {
+        tentativas--;
+    }
+}
